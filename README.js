063663566b073d43c042b90a86f930c25fdca1ec
LIZ <30482852+Gunner-12@users.noreply.github.com>
2017-10-07 19:19:37 -0400

    Create README.js

---
+++ README.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>Click the button to display an alert box:</p>
+
+<button onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {    
+    alert("WARNING! This system is for authorized users only." +'\n'+ "" +'\n'+ "By accessing this system you are consenting to complete monitoring with no expectation of privacy." +'\n'+ "" +'\n'+ "Unauthorized access or use may subject you to disciplinary action and criminal prosecution.");
+}
+</script>
+
+</body>
+</html>
